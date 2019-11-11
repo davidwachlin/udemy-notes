bb85c13452d7b9f9d509d2c2ab77f53c1d3a6d7f
David Wachlin <davidwachlin@gmail.com>
2019-11-11 13:31:54 -0500

    home route, search bar added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+    state = {
+        searchField: ''
+    }
     render() {
         return (
             <div>
                 <h1>Home</h1>
+                <form>
+                <input type="text"></input>
+                <button>Search</button>
+                </form>
             </div>
         )
     }
